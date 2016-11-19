90d2655ba1b976bbede420861d1ee751ae1c1a0f
Kevin Viscafe <kevinviscafe@gmail.com>
2016-11-19 02:28:32 -0400

    Player Basics

---
+++ core/src/com/epicness/tanks/gamelogic/GameMaster.java
@@ -0,0 +1,8 @@
+package com.epicness.tanks.gamelogic;
+
+/**
+ * Created by Kevin Viscafe on 19/11/2016.
+ */
+public class GameMaster {
+
+}
--- core/src/com/epicness/tanks/gamelogic/Player.java
+++ core/src/com/epicness/tanks/gamelogic/Player.java
@@ -1,6 +1,7 @@
 package com.epicness.tanks.gamelogic;
 
 import com.badlogic.gdx.graphics.Color;
+import com.epicness.tanks.placeables.Placeable;
 
 /**
  * Created by Kevin Viscafe on 19/11/2016.
@@ -9,6 +10,8 @@ public class Player {
 
     private Color color;
     private int team;
+    private int money;
+
 
     public int getTeam() {
         return team;
@@ -24,5 +27,17 @@ public class Player {
 
     public void setColor(Color color) {
         this.color = color;
+    }
+
+    public int getMoney() {
+        return money;
+    }
+
+    public void setMoney(int money) {
+        this.money = money;
+    }
+
+    public void removePlaceable(Placeable placeable) {
+
     }
 }
