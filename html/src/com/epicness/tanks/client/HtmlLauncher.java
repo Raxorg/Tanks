eac3702df494e3d72bc1810e999fb7aeb7b60ef8
Raxorg <luchox95@hotmail.com>
2016-11-19 00:49:01 -0400

    Initial commit

---
+++ html/src/com/epicness/tanks/client/HtmlLauncher.java
@@ -0,0 +1,19 @@
+package com.epicness.tanks.client;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import com.epicness.tanks.Tanks;
+
+public class HtmlLauncher extends GwtApplication {
+
+        @Override
+        public GwtApplicationConfiguration getConfig () {
+                return new GwtApplicationConfiguration(480, 320);
+        }
+
+        @Override
+        public ApplicationListener createApplicationListener () {
+                return new Tanks();
+        }
+}
